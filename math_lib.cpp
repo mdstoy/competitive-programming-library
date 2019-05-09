9fd23fafc9db79eb3802c1437510c4d03bc8a6c0
mdstoy <toyofyou.me@gmail.com>
2019-05-09 21:52:13 +0900

    add check prime number

--- math_lib.cpp
+++ math_lib.cpp
@@ -9,9 +9,34 @@ long long lcm(long long m, long long n) {
     return m * n / gcd(m, n);
 }
 
+bool isPrime(long long n) {
+    if (n < 2) {
+        return false;
+    } else if (n == 2) {
+        return true;
+    } else if (n % 2 == 0) {
+        return false;
+    }
+ 
+    for (long long i = 3; i <= sqrt(n); i += 2) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
 int main() {
-    // expect: 4
+    // expected: 4
     cout << gcd(8, 12) << endl;
-    // expect: 24 
+    // expected: 24 
     cout << lcm(8, 12) << endl;
+
+    // expected: 2 3 5 7 11 13 17 19....
+    for (int i = 1; i <= 100; i++) {
+        if (isPrime(i)) {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
 }
