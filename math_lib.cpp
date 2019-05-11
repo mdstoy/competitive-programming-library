5fbc29681ef1bddddeba4c5576683bf17489e9f5
mdstoy <toyofyou.me@gmail.com>
2019-05-12 01:59:28 +0900

    add listing divisors

--- math_lib.cpp
+++ math_lib.cpp
@@ -26,6 +26,19 @@ bool is_prime(long long n) {
     return true;
 }
 
+vector<long long> divisors(long long n) {
+    vector<long long> v;
+    for (long long i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            v.push_back(i);
+            if (i * i != n) {
+                v.push_back(n / i);
+            }
+        }
+    }
+    return v;
+}
+
 int main() {
     // expected: 4
     cout << gcd(8, 12) << endl;
@@ -37,6 +50,13 @@ int main() {
         if (is_prime(i)) {
             cout << i << " ";
         }
+    }
+    cout << endl;
+
+    // expected: 1 10 2 5
+    auto v = divisor(10);
+    for (auto x : v) {
+        cout << x << " ";
     }
     cout << endl;
 }
