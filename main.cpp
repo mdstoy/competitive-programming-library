486e99fbb9db450f385cdf9ab099ad64e96f326c
mdstoy <toyofyou.me@gmail.com>
2019-06-01 19:42:19 +0900

    add ch(max|min)

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@ using ll = long long;
 #define FOR(i, m, n) for (int i = (m); i < (n); i++)
 #define REP(i, n) FOR(i, 0, n)
 #define ALL(c) (c).begin(), (c).end()
+template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b; return true;} return false;}
+template<class T> inline bool chmax(T& a, T b) {if (a < b) {a = b; return true;} return false;}
 const int MOD = 1000000007;
 
 int main() {
