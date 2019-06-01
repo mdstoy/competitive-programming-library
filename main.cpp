fc8769702f135ff6c74accb5a6ce6982891078ef
mdstoy <toyofyou.me@gmail.com>
2019-06-01 15:52:28 +0900

    add REP macro

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 
 using ll = long long;
+#define FOR(i, m, n) for (int i = (m); i < (n); i++)
+#define REP(i, n) FOR(i, 0, n)
 #define ALL(c) (c).begin(), (c).end()
 const int MOD = 1000000007;
 
