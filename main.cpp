b5545069aa0344cc36b6fb802cf40ac2e1ce3f3a
mdstoy <toyofyou.me@gmail.com>
2019-08-02 00:14:06 +0900

    add rep1 macro

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 using ll = long long;
 #define FOR(i, m, n) for (int i = (m); i < (n); i++)
 #define REP(i, n) FOR(i, 0, n)
+#define REP1(i, n) FOR(i, 1, (n) + 1)
 #define ALL(c) (c).begin(), (c).end()
 template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b; return true;} return false;}
 template<class T> inline bool chmax(T& a, T b) {if (a < b) {a = b; return true;} return false;}
