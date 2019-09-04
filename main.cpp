b82f87710a33f5468c45fca50162a37069257582
mdstoy <toyofyou.me@gmail.com>
2019-09-05 00:50:15 +0900

    add INF value

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ using ll = long long;
 template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b; return true;} return false;}
 template<class T> inline bool chmax(T& a, T b) {if (a < b) {a = b; return true;} return false;}
 const int MOD = 1000000007;
+const int INF = 1000000001;
 
 int main() {
 
