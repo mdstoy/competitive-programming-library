264fa95b621d7bf3b13012b2a3570239d0625b73
mdstoy <toyofyou.me@gmail.com>
2019-10-27 00:00:45 +0900

    little fix

--- lib/warshall_floyd.cpp
+++ lib/warshall_floyd.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 #define FOR(i, m, n) for (int i = (m); i < (n); i++)
 #define REP(i, n) FOR(i, 0, (n))
+template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b; return true;} return false;}
 
 // already accepted at https://atcoder.jp/contests/abc143/submissions/8052790
 
@@ -13,5 +14,5 @@ template<typename T> vector<vector<T>> pre_warshall_floyd(int n, T inf) {
 }
 
 template<typename T> void warshall_floyd(vector<vector<T>>& g) {
-    REP(k, n) REP(i, n) REP(j, n) g[i][j] = min(g[i][j] ,g[i][k] + g[k][j]);
+    REP(k, n) REP(i, n) REP(j, n) chmin(g[i][j] ,g[i][k] + g[k][j]);
 }
