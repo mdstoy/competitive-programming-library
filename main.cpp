658be67930616129226825bd5fd2a1349b8f458d
mdstoy <toyofyou.me@gmail.com>
2019-10-26 23:24:42 +0900

    add size macro

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ using ll = long long;
 #define REPR(i, n) FORR(i, (n) - 1, 0)
 #define REP1(i, n) FOR(i, 1, (n) + 1)
 #define ALL(c) (c).begin(), (c).end()
+#define sz(v) (int)v.size()
 template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b; return true;} return false;}
 template<class T> inline bool chmax(T& a, T b) {if (a < b) {a = b; return true;} return false;}
 const int MOD = 1000000007;
