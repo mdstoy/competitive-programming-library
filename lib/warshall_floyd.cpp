107e5c60fba5c68764a19a82513aba060d05a27d
mdstoy <toyofyou.me@gmail.com>
2019-12-02 15:59:31 +0900

    little fix : missing graph size

--- lib/warshall_floyd.cpp
+++ lib/warshall_floyd.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 #define FOR(i, m, n) for (int i = (m); i < (n); i++)
 #define REP(i, n) FOR(i, 0, (n))
+#define sz(v) (int)v.size()
 template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b; return true;} return false;}
 
 // already accepted at https://atcoder.jp/contests/abc143/submissions/8052790
@@ -14,5 +15,6 @@ template<typename T> vector<vector<T>> pre_warshall_floyd(int n, T inf) {
 }
 
 template<typename T> void warshall_floyd(vector<vector<T>>& g) {
+    int n = sz(g);
     REP(k, n) REP(i, n) REP(j, n) chmin(g[i][j] ,g[i][k] + g[k][j]);
 }
