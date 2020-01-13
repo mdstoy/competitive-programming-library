81c04a63bd830456881afce018569a0fea192046
mdstoy <toyofyou.me@gmail.com>
2020-01-13 13:38:31 +0900

    add utility

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@ using ld = long double;
 #define REP1(i, n) FOR(i, 1, (n) + 1)
 #define REPS(c, s) for (char c : s)
 #define ALL(c) (c).begin(), (c).end()
+#define SORT(c) sort(ALL(c))
+#define REV(c) reverse(ALL(c))
 #define sz(v) (int)v.size()
 template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b; return true;} return false;}
 template<class T> inline bool chmax(T& a, T b) {if (a < b) {a = b; return true;} return false;}
