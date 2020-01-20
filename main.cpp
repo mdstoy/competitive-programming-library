58b94e9ddfcea1851f0f85467ff9d13264b25259
mdstoy <toyofyou.me@gmail.com>
2020-01-20 23:34:34 +0900

    change new line constant to macro for compatibility

--- main.cpp
+++ main.cpp
@@ -13,12 +13,12 @@ using ld = long double;
 #define SORT(c) sort(ALL(c))
 #define REV(c) reverse(ALL(c))
 #define sz(v) (int)v.size()
+#define endl '\n';
 template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b; return true;} return false;}
 template<class T> inline bool chmax(T& a, T b) {if (a < b) {a = b; return true;} return false;}
 const int MOD = 1000000007;
 const int INF = 1000000001;
 const ll LINF = 1000000001000000001LL;
-const char EOL = '\n';
  
 void solve();
  
