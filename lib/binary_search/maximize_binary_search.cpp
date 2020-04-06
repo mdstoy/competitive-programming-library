e0f57ce373fa4b65e6e801f01218200ccaaa104f
mdstoy <toyofyou.me@gmail.com>
2020-04-06 23:02:27 +0900

    functions can receive any arguments

--- lib/binary_search/maximize_binary_search.cpp
+++ lib/binary_search/maximize_binary_search.cpp
@@ -1,29 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// already accepted at https://atcoder.jp/contests/exawizards2019/submissions/8652435
+// already accepted at https://atcoder.jp/contests/exawizards2019/submissions/11618977
 // usage : BinarySearch<type> bs(left, right, lambda);
 // ex : BinarySearch<ll> bs(-1LL, LINF, lambda);
 
-template<typename D> class BinarySearch {
+template<typename D, class... A> class BinarySearch {
     D left;
     D right;
-    function<bool(D)> func;
+    function<bool(D, A...)> func;
 
     public:
-        BinarySearch(D l, D r, function<bool(D)> f) {
+        BinarySearch(D l, D r, function<bool(D, A...)> f) {
             left = l;
             right = r;
             func = f;
         }
 
-        D calc() {
+        D calc(A... args) {
             while (abs(right - left) > 1) {
-                D m = (left + right) / 2;
-                if (func(m)) {
-                    left = m;
+                D middle = (left + right) / 2;
+                if (func(middle, args...)) {
+                    left = middle;
                 } else {
-                    right = m;
+                    right = middle;
                 }
             }
             return left;
--- lib/binary_search/minimize_binary_search.cpp
+++ lib/binary_search/minimize_binary_search.cpp
@@ -1,29 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// already accepted at https://atcoder.jp/contests/exawizards2019/submissions/8652435
-// usage : BinarySearch<type> bs(left, right, lambda);
-// ex : BinarySearch<ll> bs(-1LL, LINF, lambda);
+// already accepted at https://atcoder.jp/contests/judge-update-202004/submissions/11618809
+// and https://atcoder.jp/contests/exawizards2019/submissions/11618977
+// usage : BinarySearch<type[, type...]> bs(left, right, lambda);
+// ex : BinarySearch<ll, ll> bs(-1LL, LINF, lambda);
 
-template<typename D> class BinarySearch {
+template<typename D, class... A> class BinarySearch {
     D left;
     D right;
-    function<bool(D)> func;
+    function<bool(D, A...)> func;
 
     public:
-        BinarySearch(D l, D r, function<bool(D)> f) {
+        BinarySearch(D l, D r, function<bool(D, A...)> f) {
             left = l;
             right = r;
             func = f;
         }
 
-        D calc() {
+        D calc(A... args) {
             while (abs(right - left) > 1) {
-                D m = (left + right) / 2;
-                if (func(m)) {
-                    right = m;
+                D middle = (left + right) / 2;
+                if (func(middle, args...)) {
+                    right = middle;
                 } else {
-                    left = m;
+                    left = middle;
                 }
             }
             return right;
