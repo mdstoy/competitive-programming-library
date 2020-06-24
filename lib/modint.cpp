77c5a5d7ecbe8ae4032ef9043f28d0ca71ff4420
mdstoy <toyofyou.me@gmail.com>
2020-06-24 23:40:32 +0900

    add modint (under construction...)

---
+++ lib/modint.cpp
@@ -0,0 +1,51 @@
+using ll = long long;
+
+class mint {
+        ll value;
+        int mod;
+    public:
+        constexpr mint(ll src = 0, int m = 1000000007) noexcept : value((src % m + m) % m), mod(m) {}
+
+        constexpr mint &operator+=(const mint rhs) noexcept {
+            value += rhs.value;
+            if (value >= mod) value -= mod;
+            return *this;
+        }
+
+        constexpr mint &operator-=(const mint rhs) noexcept {
+            if (value < rhs.value) value += mod;
+            value -= rhs.value;
+            return *this;
+        }
+
+        constexpr mint &operator*=(const mint rhs) noexcept {
+            value = value * rhs.value % mod;
+            return *this;
+        }
+        
+        constexpr mint &operator/=(mint rhs) noexcept {
+            ll exp = mod - 2;
+            while (exp) {
+                if (exp % 2) *this *= rhs;
+                rhs *= rhs;
+                exp /= 2;
+            }
+            return *this;
+        }
+
+        constexpr mint operator+(const mint rhs) const noexcept {
+            return mint(*this) += rhs;
+        }
+        
+        constexpr mint operator-(const mint rhs) const noexcept {
+            return mint(*this) -= rhs;
+        }
+  
+        constexpr mint operator*(const mint rhs) const noexcept {
+            return mint(*this) *= rhs;
+        }
+       
+        constexpr mint operator/(const mint rhs) const noexcept {
+            return mint(*this) /= rhs;
+        }
+}
