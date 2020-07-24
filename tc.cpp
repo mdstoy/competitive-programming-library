606a2a2e5c0a4f2617332a9a79b22c7a0af876de
mdstoy <toyofyou.me@gmail.com>
2020-07-24 15:58:58 +0900

    add template for TopCoder

---
+++ tc.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+using ll = long long;
+using ld = long double;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vvvi = vector<vector<vector<int>>>;
+using vl = vector<ll>;
+using vvl = vector<vector<ll>>;
+using vvvl = vector<vector<vector<ll>>>;
+using vs = vector<string>;
+using vb = vector<bool>;
+#define FOR(i, m, n) for (int i = (m); i < (n); i++)
+#define FORR(i, m, n) for (int i = (m); i >= (n); i--)
+#define REP(i, n) FOR(i, 0, (n))
+#define REPR(i, n) FORR(i, (n) - 1, 0)
+#define REP1(i, n) FOR(i, 1, (n) + 1)
+#define REPS(c, s) for (char c : s)
+#define ALL(c) (c).begin(), (c).end()
+#define SORT(c) sort(ALL(c))
+#define REV(c) reverse(ALL(c))
+#define sz(v) (int)v.size()
+#define endl '\n'
+template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b; return true;} return false;}
+template<class T> inline bool chmax(T& a, T b) {if (a < b) {a = b; return true;} return false;}
+const int MOD = 1000000007;
+const int INF = 1000000001;
+const ll LINF = 1000000001000000001LL;
+
+class ClassName {
+    public:
+    int MethodName() {
+
+    }
+};
