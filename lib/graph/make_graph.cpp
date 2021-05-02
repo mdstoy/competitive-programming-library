1a8955b64d25b43594cc14a9a43163ca4387a674
mdstoy <toyofyou.me@gmail.com>
2021-05-02 20:48:05 +0900

    refine making graph function

--- lib/graph/make_graph.cpp
+++ lib/graph/make_graph.cpp
@@ -1,11 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// verifying : https://atcoder.jp/contests/typical90/submissions/22039607
+// verifying : https://atcoder.jp/contests/typical90/submissions/22261677
 
-vector<vector<int>> make_tree(int n, bool origin1 = true) {
+vector<vector<int>> make_graph(int n, int m, bool origin1 = true) {
     vector<vector<int>> g(n, vector<int>());
-    for (int i = 0; i < n - 1; i++) {
+    for (int i = 0; i < m; i++) {
         int u, v;
         cin >> u >> v;
         if (origin1) {
