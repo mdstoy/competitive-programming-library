7920e78503d3ba3d3c16f3d5e465842442fb726a
mdstoy <toyofyou.me@gmail.com>
2021-07-11 20:16:59 +0900

    useful anytime, anywhere

--- lib/combination/pascals_triangle.cpp
+++ lib/combination/pascals_triangle.cpp
@@ -1,14 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using ll = long long;
-#define FOR(i, m, n) for (int i = (m); i < (n); i++)
-#define REP(i, n) FOR(i, 0, (n))
-
 // combi[n][k] means nCk
-vector<vector<ll>> pascals_triangle(int n) {
-    vector<vector<ll>> combi(n + 1, vector<ll>(n + 1));
-    REP(i, n + 1) REP(j, i + 1) {
+vector<vector<long long>> pascals_triangle(int n) {
+    vector<vector<long long>> combi(n + 1, vector<long long>(n + 1));
+    for (int i = 0; i <= n; i++) for (int j = 0; j <= i; j++) {
         if (j == 0 || j == i) combi[i][j] = 1LL;
         else combi[i][j] = combi[i - 1][j - 1] + combi[i - 1][j];
     }
@@ -18,8 +14,8 @@ vector<vector<ll>> pascals_triangle(int n) {
 int main() {
     // Pascal's triangle
     auto tri = pascals_triangle(50);
-    REP(i, 21) {
-        REP(j, i + 1) {
+    for (int i = 0; i < 21; i++) {
+        for (int j = 0; j <= i; j++) {
             cout << tri[i][j] << " ";
         }
         cout << endl;
