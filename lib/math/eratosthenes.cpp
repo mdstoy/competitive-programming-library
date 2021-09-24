79dbadda5d176189e5ddda275d957281982d1c87
mdstoy <toyofyou.me@gmail.com>
2021-09-25 00:02:29 +0900

    add interval sieve of Eratosthenes

--- lib/math/eratosthenes.cpp
+++ lib/math/eratosthenes.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// sieve of Eratosthenes
 template<typename T> vector<bool> eratosthenes(T n) {
     vector<bool> is_prime(n + 1, true);
     is_prime[0] = false;
@@ -10,6 +11,21 @@ template<typename T> vector<bool> eratosthenes(T n) {
         if (!is_prime[i]) continue;
         for (T j = i * 2; j <= n; j += i) {
             is_prime[i] = false;
+        }
+    }
+    return is_prime;
+}
+
+// interval ([m, n]) sieve of Eratosthenes
+// prerequisite: m <= n
+template<typename T> vector<bool> eratosthenes(T m, T n) {
+    vector<bool> is_prime(n + 1 - m, true);
+
+    for (T i = 2; i * i <= n; i++) {
+        for (T j = (m + i - 1) / i; i * j <= n; j++) {
+            // keep state when it is a prime number
+			if (i == i * j) continue;
+            is_prime[i * j - m] = false;
         }
     }
     return is_prime;
