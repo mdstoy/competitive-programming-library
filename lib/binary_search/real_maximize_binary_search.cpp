f99b415df9cbc35836a951c00a59dcc01420108e
mdstoy <toyofyou.me@gmail.com>
2021-09-26 17:14:49 +0900

    fix binary search with real number

--- lib/binary_search/real_maximize_binary_search.cpp
+++ lib/binary_search/real_maximize_binary_search.cpp
@@ -1,27 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// already accepted at https://atcoder.jp/contests/exawizards2019/submissions/11618977
+// already accepted at https://algo-method.com/submissions/87640
 // usage : BinarySearch<type> bs(left, right, lambda);
-// ex : BinarySearch<ll> bs(-1LL, LINF, lambda);
+// ex : BinarySearch<double> bs(0.0, 1000.0, lambda);
 
 template<typename D, class... A> class BinarySearch {
     D left;
     D right;
     function<bool(D, A...)> func;
+    D eps;
     // NOTE: adjust the limiter
-    int limiter = 100;
+    int limiter;
 
     public:
-        BinarySearch(D l, D r, function<bool(D, A...)> f) {
+        BinarySearch(D l, D r, function<bool(D, A...)> f, D e = DBL_EPSILON, int limit = 100) {
             left = l;
             right = r;
             func = f;
+            eps = e;
+            limiter = limit;
         }
 
         D calc(A... args) {
             // NOTE: adjust the epsilon
-            while (right - left > DBL_EPSILON) {
+            while (right - left > eps) {
                 D middle = (left + right) / 2;
                 if (func(middle, args...)) {
                     left = middle;
--- lib/binary_search/real_minimize_binary_search.cpp
+++ lib/binary_search/real_minimize_binary_search.cpp
@@ -1,28 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// already accepted at https://atcoder.jp/contests/judge-update-202004/submissions/11618809
-// and https://atcoder.jp/contests/exawizards2019/submissions/11618977
+// already accepted at https://algo-method.com/submissions/87656
 // usage : BinarySearch<type[, type...]> bs(left, right, lambda);
-// ex : BinarySearch<ll, ll> bs(-1LL, LINF, lambda);
+// ex : BinarySearch<ll, ll> bs(0.0, 1000.0, lambda);
 
 template<typename D, class... A> class BinarySearch {
     D left;
     D right;
     function<bool(D, A...)> func;
+    D eps;
     // NOTE: adjust the limiter
-    int limiter = 100;
+    int limiter;
 
     public:
-        BinarySearch(D l, D r, function<bool(D, A...)> f) {
+        BinarySearch(D l, D r, function<bool(D, A...)> f, D e = DBL_EPSILON, int limit = 100) {
             left = l;
             right = r;
             func = f;
+            eps = e;
+            limiter = limit;
         }
 
         D calc(A... args) {
             // NOTE: adjust the epsilon
-            while (right - left > DBL_EPSILON) {
+            while (right - left > eps) {
                 D middle = (left + right) / 2;
                 if (func(middle, args...)) {
                     right = middle;
