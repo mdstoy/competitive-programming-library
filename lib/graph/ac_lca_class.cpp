7569849d1e43cc6595a8b5a139c3f75bb2c8ab89
mdstoy <toyofyou.me@gmail.com>
2021-09-28 16:38:19 +0900

    refine lca library: add method of answering edge

--- lib/graph/ac_lca_class.cpp
+++ lib/graph/ac_lca_class.cpp
@@ -3,26 +3,28 @@ using namespace std;
 #include <atcoder/all>
 using namespace atcoder;
 
-// already accepted at https://atcoder.jp/contests/abc014/submissions/26192425
+// already accepted:
+// distance at https://atcoder.jp/contests/abc014/submissions/26204523
+// lca edge at https://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=5918921#1
 
-int op(int a, int b) { return min(a, b); }
-int e() { return 1000000001; }
+pair<int, int> op(pair<int, int> a, pair<int, int> b) { return min(a, b); }
+pair<int, int> e() { return {1000000001, 1000000001}; }
 
 class Lca {
     vector<vector<int>> graph;
     int n;
     vector<int> mn;
-    vector<int> depth;
-    segtree<int, op, e> st;
+    vector<pair<int, int>> depth;
+    segtree<pair<int, int>, op, e> st;
 
     void dfs(int c, int p, int d) {
-        depth.emplace_back(d);
+        depth.emplace_back(d, c);
         if (mn[c] > (int)depth.size() - 1) mn[c] = (int)depth.size() - 1;
         if (p != -1 and (int)graph[c].size() == 1) return;
         for (auto e : graph[c]) {
             if (e == p) continue;
             dfs(e, c, d + 1);
-            depth.emplace_back(d);
+            depth.emplace_back(d, c);
         }
     }
 
@@ -31,23 +33,25 @@ class Lca {
             graph = g;
             n = (int)g.size();
             mn = vector<int>(n, 1000000001);
-            depth = vector<int>();
+            depth = vector<pair<int, int>>();
             dfs(0, -1, 0);
-            st = segtree<int, op, e>(depth);
+            st = segtree<pair<int, int>, op, e>(depth);
         }
 
-	// get lca of u and v
-        int prod(int u, int v) {
+        pair<int, int> prod(int u, int v) {
             int l = mn[u];
             int r = mn[v];
             if (l > r) swap(l, r);
             return st.prod(l, r + 1);
         }
 
-	// get distance from u to v
+        int lca(int u, int v) {
+            auto lca = prod(u, v);
+            return lca.second;
+        }
+
         int distance(int u, int v) {
             auto lca = prod(u, v);
-            return depth[mn[u]] - lca + depth[mn[v]] - lca + 1;
+            return depth[mn[u]].first - lca.first + depth[mn[v]].first - lca.first + 1;
         }
 };
-
