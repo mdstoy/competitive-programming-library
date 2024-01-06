ec43f598d9c39038a825b8d21567faf28f9ee34f
mdstoy <toyofyou.me@gmail.com>
2024-01-07 02:11:08 +0900

    counting indegree in this function

--- lib/graph/topological_sort_in_dictionary_order.cpp
+++ lib/graph/topological_sort_in_dictionary_order.cpp
@@ -1,11 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// verifying: https://atcoder.jp/contests/abc223/submissions/26656922
+// verifying: https://atcoder.jp/contests/abc223/submissions/49129376
 
-vector<int> topological_sort(vector<vector<int>> g, vector<int> in_depth) {
-    vector<int> ret;
+vector<int> topological_sort(vector<vector<int>> g) {
     int n = (int)g.size();
+    vector<int> in_depth(n);
+    for (int i = 0; i < n; i++) {
+        for (auto e : g[i]) {
+            in_depth[e]++;
+        }
+    }
+    vector<int> ret;
     priority_queue<int> q;
     for (int i = 0; i < n; i++) {
         if (in_depth[i] == 0) {
